73bb54231c3df914b0da7cc0e5eb231eff511530
pasly0920 <inwoo920621@gmail.com>
2023-02-10 17:09:25 +0900

    feat: base entity 추가

---
+++ src/main/java/com/gdscuos/recruit/global/common/BaseEntity.java
@@ -0,0 +1,19 @@
+package com.gdscuos.recruit.global.common;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity extends BaseTimeEntity {
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+}
---
+++ src/main/java/com/gdscuos/recruit/global/common/BaseTimeEntity.java
@@ -0,0 +1,19 @@
+package com.gdscuos.recruit.global.common;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
