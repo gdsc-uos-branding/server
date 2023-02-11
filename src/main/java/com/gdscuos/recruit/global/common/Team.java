4b1dd38efe3bd724b3cb8f7fbac8dacc943c38c6
pasly0920 <inwoo920621@gmail.com>
2023-02-11 16:22:12 +0900

    fix: team enum key 약자 -> 풀네임

--- src/main/java/com/gdscuos/recruit/global/common/Team.java
+++ src/main/java/com/gdscuos/recruit/global/common/Team.java
@@ -7,12 +7,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Team {
 
-    FRONTEND("FE", "TEAM_FRONTEND"),
-    BACKEND("BE", "TEAM_BACKEND"),
-    MOBILE("M", "TEAM_MOBILE"),
-    DATA_ML("DM", "TEAM_DATA_ML"),
-    CORE("C", "TEAM_CORE"),
-    LEAD("L", "TEAM_LEAD");
+    FRONTEND("TEAM_FRONTEND", "TEAM_FRONTEND"),
+    BACKEND("TEAM_BACKEND", "TEAM_BACKEND"),
+    MOBILE("TEAM_MOBILE", "TEAM_MOBILE"),
+    DATA_ML("TEAM_DATA_ML", "TEAM_DATA_ML"),
+    CORE("TEAM_CORE", "TEAM_CORE"),
+    LEAD("TEAM_LEAD", "TEAM_LEAD");
 
     private final String key;
     private final String title;
