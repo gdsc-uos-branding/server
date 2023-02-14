04be12ce0d512b637b7d5e349f1bb4a0c8f5f0e5
pasly0920 <inwoo920621@gmail.com>
2023-02-14 22:32:53 +0900

    fix: guest role 삭제

--- src/main/java/com/gdscuos/recruit/global/common/Role.java
+++ src/main/java/com/gdscuos/recruit/global/common/Role.java
@@ -6,12 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    GUEST("ROLE_GUEST", "손님"),
-    APPLICANT("ROLE_APPLICANT", "지원자"),
-    MEMBER("ROLE_MEMBER", "멤버"),
-    CORE("ROLE_CORE", "코어멤버"),
-    LEAD("ROLE_LEAD", "리드");
+    APPLICANT("ROLE_APPLICANT"),
+    MEMBER("ROLE_MEMBER"),
+    CORE("ROLE_CORE"),
+    LEAD("ROLE_LEAD");
 
-    private final String key;
-    private final String title;
+    private final String value;
 }
--- src/main/java/com/gdscuos/recruit/global/common/Team.java
+++ src/main/java/com/gdscuos/recruit/global/common/Team.java
@@ -7,13 +7,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Team {
 
-    FRONTEND("TEAM_FRONTEND", "TEAM_FRONTEND"),
-    BACKEND("TEAM_BACKEND", "TEAM_BACKEND"),
-    MOBILE("TEAM_MOBILE", "TEAM_MOBILE"),
-    DATA_ML("TEAM_DATA_ML", "TEAM_DATA_ML"),
-    CORE("TEAM_CORE", "TEAM_CORE"),
-    LEAD("TEAM_LEAD", "TEAM_LEAD");
+    FRONTEND("TEAM_FRONTEND"),
+    BACKEND("TEAM_BACKEND"),
+    MOBILE("TEAM_MOBILE"),
+    DATA_ML("TEAM_DATA_ML"),
+    CORE("TEAM_CORE"),
+    LEAD("TEAM_LEAD");
 
-    private final String key;
-    private final String title;
+    private final String value;
 }
