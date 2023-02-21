6d4c76375fb9fa3f4d67d8ba3915107a6f717d36
pasly0920 <inwoo920621@gmail.com>
2023-02-21 19:01:15 +0900

    feat: Global Error 처리 Handler 추가

---
+++ src/main/java/com/gdscuos/recruit/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.gdscuos.recruit.global.error;
+
+import com.gdscuos.recruit.global.error.exception.BusinessException;
+import com.gdscuos.recruit.global.error.exception.EntityNotFoundException;
+import com.gdscuos.recruit.global.error.exception.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(
+            final BusinessException exception) {
+        log.error("handleBusinessException", exception);
+        final ErrorCode errorCode = exception.getErrorCode();
+        final ErrorResponse response = ErrorResponse.from(errorCode);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleEntityNotFoundException(
+            final EntityNotFoundException exception) {
+        log.error("handleEntityNotFoundException", exception);
+        final ErrorCode errorCode = exception.getErrorCode();
+        final ErrorResponse response = ErrorResponse.from(exception.getErrorCode());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+}
