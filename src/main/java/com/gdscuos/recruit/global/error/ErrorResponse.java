c72ce80ae00996f7774f60abd37e46639e4a480c
pasly0920 <inwoo920621@gmail.com>
2023-02-21 19:00:08 +0900

    feat: Error Code 활용한 Error 응답 객체 생성

---
+++ src/main/java/com/gdscuos/recruit/global/error/ErrorResponse.java
@@ -0,0 +1,25 @@
+package com.gdscuos.recruit.global.error;
+
+import com.gdscuos.recruit.global.error.exception.ErrorCode;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+
+    private String message;
+    private int status;
+    private String code;
+
+    private ErrorResponse(final ErrorCode code) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.code = code.getCode();
+    }
+
+    public static ErrorResponse from(final ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+}
