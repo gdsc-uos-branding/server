8b4bd9cff29e2e827fbc052c864c5663cc76ef27
pasly0920 <inwoo920621@gmail.com>
2023-02-23 12:41:35 +0900

    feat: error code 오타 및 순번 오류 해결

--- src/main/java/com/gdscuos/recruit/global/error/exception/ErrorCode.java
+++ src/main/java/com/gdscuos/recruit/global/error/exception/ErrorCode.java
@@ -9,22 +9,23 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE("C01", "Invalid Input Value.", HttpStatus.BAD_REQUEST.value()),
     METHOD_NOT_ALLOWED("C02", "Invalid Method Type.", HttpStatus.METHOD_NOT_ALLOWED.value()),
     ENTITY_NOT_FOUND("C03", "Entity Not Found.", HttpStatus.BAD_REQUEST.value()),
-    INTERNAL_SERVER_ERROR("C04", "Server Error.", HttpStatus.INTERNAL_SERVER_ERROR.value()),
+    INTERNAL_SERVER_ERROR("C04", "Internal Server Error.",
+            HttpStatus.INTERNAL_SERVER_ERROR.value()),
     INVALID_TYPE_VALUE("C05", " Invalid Type Value.", HttpStatus.BAD_REQUEST.value()),
 
     // User
     USER_ACCESS_DENIED("A01", "User Access is Denied.", HttpStatus.UNAUTHORIZED.value()),
     USER_NOT_FOUND("A02", "User is not Found.", HttpStatus.BAD_REQUEST.value()),
-    EMAIL_DUPLICATION("A03", "Email is Duplication", HttpStatus.BAD_REQUEST.value()),
+    EMAIL_DUPLICATION("A03", "Email is Duplicated.", HttpStatus.BAD_REQUEST.value()),
 
     // User - Token
     ACCESS_TOKEN_EXPIRED("AT01", "Access Token is Expired", HttpStatus.UNAUTHORIZED.value()),
     ACCESS_TOKEN_INVALID("AT02", "Access Token is Invalid.", HttpStatus.UNAUTHORIZED.value()),
     ACCESS_TOKEN_NOT_FOUND_IN_HEADER("AT03", "Access Token is not Found in Header.",
             HttpStatus.UNAUTHORIZED.value()),
-    REFRESH_TOKEN_NOT_FOUND("AT05", "Refreshed Token is not Found.",
+    REFRESH_TOKEN_NOT_FOUND("AT04", "Refreshed Token is not Found.",
             HttpStatus.UNAUTHORIZED.value()),
-    REFRESH_TOKEN_EXPIRED("AT06", "Refreshed Token is Expired.", HttpStatus.UNAUTHORIZED.value()),
+    REFRESH_TOKEN_EXPIRED("AT05", "Refreshed Token is Expired.", HttpStatus.UNAUTHORIZED.value()),
 
     // external API
     EXTERNAL_API_FAILED("E01", "External API Request is failed.",
