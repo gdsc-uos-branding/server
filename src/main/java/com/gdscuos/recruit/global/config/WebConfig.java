b3119a5c59f668f8a932b1f3bdc763aa7e349e69
Garfield <iju1633@gmail.com>
2023-03-12 17:42:08 +0900

    - Fix: interceptor에서 exclude하는 로직 대신 add 로직을 활용하여 인터셉터를 적용할 url만 적용 - Feat: 비로그인 시, 소셜 로그인 화면으로 리다이렉트되지 않도록 설정

--- src/main/java/com/gdscuos/recruit/global/config/WebConfig.java
+++ src/main/java/com/gdscuos/recruit/global/config/WebConfig.java
@@ -2,30 +2,27 @@ package com.gdscuos.recruit.global.config;
 
 import com.gdscuos.recruit.global.util.LoginCheckInterceptor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "login")
 public class WebConfig implements WebMvcConfigurer {
 
-    private List<String> whiteListURLs;
+    private List<String> whiteListURLs = new ArrayList<>();
 
-    public WebConfig(List<String> whiteListURLs) {
+    public void setWhiteListURLs(List<String> whiteListURLs) {
         this.whiteListURLs = whiteListURLs;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor()) // 인터셉터 등록
-                .order(1) // 낮을 수록 먼저 호출
-                .excludePathPatterns(whiteListURLs.toArray(new String[0])); // 인터셉터를 적용 안할 url 패턴
-    }
-
-    public void setWhiteListURLs(List<String> whiteListURLs) {
-        this.whiteListURLs = whiteListURLs;
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .addPathPatterns(whiteListURLs.toArray(new String[0]))
+                .order(1);
     }
 }
