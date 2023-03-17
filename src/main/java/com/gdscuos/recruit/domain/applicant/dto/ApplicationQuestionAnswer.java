3e7d083852ba066a5b07018ed8cd663cd6830d0c
pasly0920 <inwoo920621@gmail.com>
2023-03-17 21:05:28 +0900

    feat: 지원서 조회 관련 dto 추가

---
+++ src/main/java/com/gdscuos/recruit/domain/applicant/dto/ApplicationQuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.gdscuos.recruit.domain.applicant.dto;
+
+import com.gdscuos.recruit.domain.applicant.domain.ApplicationQuestion;
+import com.gdscuos.recruit.global.common.Team;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ApplicationQuestionAnswer {
+
+    private Team questionTeam;
+    private String question;
+    private String answer;
+    private Boolean required;
+    private Integer maxLength;
+
+    public ApplicationQuestionAnswer(ApplicationQuestion applicationQuestion) {
+        this.questionTeam = applicationQuestion.getTeam();
+        this.question = applicationQuestion.getQuestion();
+        this.answer = applicationQuestion.getAnswer();
+        this.required = applicationQuestion.getRequired();
+        this.maxLength = applicationQuestion.getMaxLength();
+    }
+}
