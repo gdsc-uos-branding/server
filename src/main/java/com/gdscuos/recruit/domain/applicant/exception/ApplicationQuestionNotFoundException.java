042742e2603ece78646aeb758f37c8981ca8260b
pasly0920 <inwoo920621@gmail.com>
2023-03-17 21:05:53 +0900

    feat: 지원서 미존재시 Exception 정의

--- src/main/java/com/gdscuos/recruit/domain/applicant/exception/ApplicationQuestionNotFoundException.java
+++ src/main/java/com/gdscuos/recruit/domain/applicant/exception/ApplicationQuestionNotFoundException.java
@@ -6,6 +6,6 @@ import com.gdscuos.recruit.global.error.exception.ErrorCode;
 public class ApplicationQuestionNotFoundException extends EntityNotFoundException {
 
     public ApplicationQuestionNotFoundException() {
-        super(ErrorCode.Application_Question_NOT_FOUND);
+        super(ErrorCode.APPLICATION_QUESTION_NOT_FOUND);
     }
 }
