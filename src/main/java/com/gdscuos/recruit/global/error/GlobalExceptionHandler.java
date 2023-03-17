c853d56f6866fed83e1f1d65a2696025ec6856d8
Garfield <iju1633@gmail.com>
2023-03-17 20:37:48 +0900

    Revert "- Feat : 어드민 기능 구현 (유저 조회, 권한 변경, 회원 탈퇴, 팀 소개 수정)"
    This reverts commit 4c6d4bf26795f0ffd2685e037dad78174204af55.

--- src/main/java/com/gdscuos/recruit/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/gdscuos/recruit/global/error/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.gdscuos.recruit.global.error;
 
-import com.gdscuos.recruit.global.error.exception.AccessDeniedException;
 import com.gdscuos.recruit.global.error.exception.BusinessException;
 import com.gdscuos.recruit.global.error.exception.EntityNotFoundException;
 import com.gdscuos.recruit.global.error.exception.ErrorCode;
@@ -17,15 +16,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(AccessDeniedException.class)
-    protected ResponseEntity<ErrorResponse> handleAccessDeniedException(
-            final AccessDeniedException exception) {
-        log.error("handleAccessDeniedException", exception);
-        final ErrorCode errorCode = exception.getErrorCode();
-        final ErrorResponse response = ErrorResponse.from(errorCode);
-        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
-    }
 
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ErrorResponse> handleBusinessException(
