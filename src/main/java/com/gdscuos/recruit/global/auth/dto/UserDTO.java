4c6d4bf26795f0ffd2685e037dad78174204af55
Garfield <iju1633@gmail.com>
2023-03-17 19:25:57 +0900

    - Feat : 어드민 기능 구현 (유저 조회, 권한 변경, 회원 탈퇴, 팀 소개 수정)

--- src/main/java/com/gdscuos/recruit/global/auth/dto/UserDTO.java
+++ src/main/java/com/gdscuos/recruit/global/auth/dto/UserDTO.java
@@ -1,19 +1,37 @@
 package com.gdscuos.recruit.global.auth.dto;
 
+import com.gdscuos.recruit.global.common.Role;
+import com.gdscuos.recruit.global.common.Team;
 import com.gdscuos.recruit.global.common.User;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserDTO {
 
     private long userId;
     private String username;
     private String email;
+    private Role role;
+    private Team team;
 
-    // Entity -> DTO
+    // Entity -> DTO mapping
     public UserDTO(User entity) {
         this.userId = entity.getId();
         this.username = entity.getUsername();
         this.email = entity.getEmail();
+        this.role = entity.getRole();
+    }
+
+    // Entity -> return DTO
+    public static UserDTO from(User user) {
+        return UserDTO.builder()
+                .userId(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .team(user.getTeam())
+                .build();
     }
 }
